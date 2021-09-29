b00095d67cb07909db7c6b1ec1c91c2baa1ca29a
LV Network <nilson@techlabs.net.br>
2021-09-29 11:31:21 -0300

    Atualizacao

--- install_controller_1804.sh
+++ install_controller_1804.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
 # Originalmente Criado por Robson Vaamonde
-# Data de criação: 06/01/2021
-
-# Adptação e melhorias no Scriot por TechLabs Technology
-# Data de atualização: 29/09/2021
-
-# Versão: 1.06
+# Adptação e melhorias no Script por TechLabs Technology
+# Versão: 0.06
 
 # Testado e homologado para a versão do Ubuntu Server 18.04.x LTS x64
 # Testado e homologado para a versão do Unifi Controller 6.2.x, MongoDB 3.6.x, OpenJDK e OpenJRE 8.x
@@ -15,8 +11,6 @@ UBUNTU=$(lsb_release -rs)
 LOG="/var/log/$(echo $0 | cut -d'/' -f2)"
 
 export DEBIAN_FRONTEND="noninteractive"
-
-clear
 #
 echo
 echo -e "## --- Instalação do Unifi Controller - Ubuntu Server 18.04 --- ##\n"
@@ -53,7 +47,6 @@ sleep 5
 echo -e "## --- Instalando o Unifi Controller --- ##\n"
 #
 echo -e "[ TECHLABS ] Adicionando o repositório do MongoDB"
-	#wget -qO - $KEYSRVMONGODB | apt-key add - &>> $LOG
 	wget -qO - https://www.mongodb.org/static/pgp/server-3.4.asc | apt-key add - &>> $LOG
 	cp -v arquivos/mongodb-org-3.4.list /etc/apt/sources.list.d/ &>> $LOG
 echo -e "[OK]\n"
